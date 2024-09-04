2c9050a6da5d0b51398dc903d276a07941e87757
kiendn1 <trungkienty2001@gmail.com>
2024-09-04 16:10:30 +0700

    fix

--- main.py
+++ main.py
@@ -103,9 +103,9 @@ def load_data(args):
     gen_loader, n_class = data_loader.load_data(
         args, folder_gen, 16, infinite_data_loader=True, train=True, num_workers=args.num_workers)
     source_loader, n_class = data_loader.load_data(
-        args, folder_src, 32, infinite_data_loader=True, train=True, num_workers=args.num_workers)
+        args, folder_src, 16, infinite_data_loader=True, train=True, num_workers=args.num_workers)
     target_train_loader, _ = data_loader.load_data(
-        args, folder_tgt, 16, infinite_data_loader=True, train=True, use_fixmatch=use_fixmatch, num_workers=args.num_workers, partial=args.pda)
+        args, folder_tgt, 32, infinite_data_loader=True, train=True, use_fixmatch=use_fixmatch, num_workers=args.num_workers, partial=args.pda)
     target_test_loader, _ = data_loader.load_data(
         args, folder_tgt, 32, infinite_data_loader=False, train=False, num_workers=args.num_workers, partial=args.pda)
     return source_loader, target_train_loader, target_test_loader, gen_loader, n_class
